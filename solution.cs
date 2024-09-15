38f6ed51041c8fb45dc3e24eab02f4a1ca4292b3
Kooreis <kooreis.official@gmail.com>
2024-09-15 13:48:32 -0500

    This update introduces the reverseList function, which is a recursive function that reverses the linked list. It takes the head of the list as a parameter and returns the head of the reversed list.

--- solution.cs
+++ solution.cs
@@ -1,4 +1,9 @@
-function ListNode(val, next) {
-    this.val = (val===undefined ? 0 : val)
-    this.next = (next===undefined ? null : next)
+function reverseList(head) {
+    if (head === null || head.next === null) {
+        return head;
+    }
+    let p = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+    return p;
 }
